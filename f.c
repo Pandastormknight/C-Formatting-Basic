3e868de527c28cd980bdd86167252a621ce3cafa
Pandastormknight <35253513+Pandastormknight@users.noreply.github.com>
2018-01-09 13:05:11 +0500

    Add files via upload

---
+++ f.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("%d\n", 300);
+
+    printf("%f\n", 3.0);
+
+    printf("%c\n", 65);
+
+    printf("Pokemon %10d\n", 300);
+
+    printf("Pokemon %10f\n", 3.0);
+
+    printf("Pokemon %10c\n", 65);
+
+    printf("%-10d Pokemon\n", 300);
+
+    printf("%-10f Pokemon\n", 3.0);
+
+    printf("%-10c Pokemon\n", 65);
+
+    printf("%.4d\n", 300);
+
+    printf("%.4f\n", 33.0);
+
+    printf("%.4c\n", 65);
+
+}
